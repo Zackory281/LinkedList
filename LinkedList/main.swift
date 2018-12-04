4b063c327cfa572009b601491ffc49917bcf7ef2
Zijin Cui <zijincui281@berkeley.edu>
2018-12-04 00:01:33 -0800

    Initial Commit

---
+++ LinkedList/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LinkedList
+//
+//  Created by Zackory Cramer on 12/4/18.
+//  Copyright © 2018 Aoil Applications. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
